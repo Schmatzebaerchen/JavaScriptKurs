2190c2ed4155e8894eaaab2373f90ce5516d6772
Schmatzebaerchen <chw1607@web.de>
2021-02-16 14:15:17 +0100

    Anzeige Tabellenkopf und -inhalt in einer Klasse

--- js/services/userService.js
+++ js/services/userService.js
@@ -4,12 +4,9 @@ class UserService {
   static init(){
     var user=UserService.createUser(1,"Herbert","xxxxxx","herbert@irgendwas.de","01.01.1900");
     //create Presentation of User
-    UserService.displayUser(user);
+    //UserService.displayUser(user);
     UserService.createTableHeader(user);
   }
-
-
-
 
 
   static createUser(id,name,password,email,birthday){
@@ -18,22 +15,27 @@ class UserService {
   }
 
 
-
   static createTableHeader(user){
     var columnNames=Object.keys(user);
     var result="";
-    for (var i=0;i<columnNames;i++){
-      var columnName="th"+columName[i]+"</th>";
+    for (var i=0;i<columnNames.length;i++){
+      var columnName="<th>"+columnNames[i]+"</th>";
       result=result+columnName;
       console.log(columnName);
     }
+    var columnValueArray=Object.values(user);
+    var result2="<tr>";
+    for (var i=0;i<columnValueArray.length;i++){
+      var columnValue="<td>"+columnValueArray[i]+"</td>";
+      result2=result2+columnValue;
+    }
     document.getElementById("header").innerHTML=result;
+    document.getElementById("content").innerHTML=result2+"</tr>";
+
   }
 
 
-
-
-
+/*
   static displayUser(user) {
     var columnValueArray=Object.values(user);
     var result="<tr>";
@@ -43,16 +45,11 @@ class UserService {
     }
     document.getElementById("content").innerHTML=result+"</tr>";
 
-      }
-
-
-
+  }
+*/
 
 
   static changePassword(oldPassword,newPassword,repeatedNewPassword){
     return "passwordChanged";
   }
-
-
-
 }

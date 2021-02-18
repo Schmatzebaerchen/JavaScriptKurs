5d4a344759f6f9381ce408f96324f85ff0d38ea2
Schmatzebaerchen <chw1607@web.de>
2021-02-18 08:45:29 +0100

    Stand 17.02.2021

--- js/services/userService.js
+++ js/services/userService.js
@@ -1,55 +1,95 @@
 class UserService {
 
-
   static init(){
-    var user=UserService.createUser(1,"Herbert","xxxxxx","herbert@irgendwas.de","01.01.1900");
-    //create Presentation of User
-    //UserService.displayUser(user);
-    UserService.createTableHeader(user);
+
+console.log(document);
+
+    // load user data from spource
+    var user = UserService.createUser(1, "Herbert", "xxxxx", "herbert@irgendwas.de", "01.01.1900");
+    var user2 = UserService.createUser(2, "Franz", "xxxxx", "franz@irgendwas.de", "01.01.1900");
+    var user3 = UserService.createUser(3, "Hugo", "xxxxx", "hugo@irgendwas.de", "01.01.1900");
+    var user4 = UserService.createUser(4, "Ingo", "xxxxx", "ingo@irgendwas.de", "01.01.1900");
+
+    var address1 = new Address("Doberaner Platz","18057","Rostock");
+
+
+    user.address = address1;
+
+    var userList = new UserList();
+    userList.addUser(user);
+    userList.addUser(user2);
+    userList.addUser(user3);
+    userList.addUser(user4);
+
+
+    UserService.showUsers(userList.users);
+
+    console.log(userList);
+
   }
 
-
-  static createUser(id,name,password,email,birthday){
-    var user=new User(id,name,password,email,birthday);
+  static createUser(id, name, password, email, birthday){
+    var user = new User(id, name, password, email, birthday);
     return user;
   }
 
 
-  static createTableHeader(user){
-    var columnNames=Object.keys(user);
-    var result="";
-    for (var i=0;i<columnNames.length;i++){
-      var columnName="<th>"+columnNames[i]+"</th>";
-      result=result+columnName;
-      console.log(columnName);
-    }
-    var columnValueArray=Object.values(user);
-    var result2="<tr>";
-    for (var i=0;i<columnValueArray.length;i++){
-      var columnValue="<td>"+columnValueArray[i]+"</td>";
-      result2=result2+columnValue;
-    }
-    document.getElementById("header").innerHTML=result;
-    document.getElementById("content").innerHTML=result2+"</tr>";
+  static showUsers(userList){
+    var result = [];
+     var resultContent="";
+     for (var i = 0; i < userList.length; i++) {
+       resultContent = resultContent + "<tr>"
+       let user = userList[i];
+       result = UserService.createTableHeaderAndContent(user);
+       resultContent = resultContent + result[1] + "</tr>";
+     }
 
+     document.getElementById("header").innerHTML = result[0];
+     document.getElementById("content").innerHTML = resultContent;
   }
 
 
-/*
-  static displayUser(user) {
-    var columnValueArray=Object.values(user);
-    var result="<tr>";
-    for (var i=0;i<columnValueArray.length;i++){
-      var columnValue="<td>"+columnValueArray[i]+"</td>";
-      result=result+columnValue;
+  static createTableHeaderAndContent(user){
+    var columnNames = Object.keys(user);
+    var columnValueArray = Object.values(user);
+
+    var resultHeader = "";
+    var resultContent = "<tr>";
+
+    for (var i = 0; i < columnNames.length; i++) {
+      var wrongColumnName = columnNames[i];
+      var correctColumnName = wrongColumnName.substring(0,1).toUpperCase()+wrongColumnName.substring(1)
+
+      var columnName = "<th>" + correctColumnName + "</th>";
+      resultHeader = resultHeader + columnName;
+
+      var columnValue = UserService.generateTdLine(columnValueArray[i]);
+
+      resultContent = resultContent + columnValue;
     }
-    document.getElementById("content").innerHTML=result+"</tr>";
 
+    return [resultHeader, resultContent];
   }
-*/
+
+ static generateTdLine(value){
+   var columnValue ="";
+   if (value instanceof Address){
+     columnValue = "<td>" + value.postCode + "</td>";
+   }
+   else {
+     if(value === undefined){
+         columnValue = "<td>Keine Adresse angegeben!</td>";
+     }
+     else{
+         columnValue = "<td>" + value + "</td>";
+     }
+   }
+
+   return columnValue;
+ }
 
 
-  static changePassword(oldPassword,newPassword,repeatedNewPassword){
+  static changePassword(oldPassword, newPassword, repeatedNewPassword){
     return "passwordChanged";
   }
 }
